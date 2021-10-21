f32c2f42751a8208b86e9669066b09b77ab63b63
Elia Brown <brownelijah143@gmail.com>
2021-10-21 17:14:46 -0600

    gallery component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -6,19 +6,14 @@ import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
-    //renders About test
-
-    // First Test
-    it('renders', () => {
-        render(<About />)
-    });
-
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    }
-    )
-  })
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
